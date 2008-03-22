6bad6c840f7124e41031f52e33093c158019ddc5
mathieu.barbin <mathieu.barbin@64936ccc-2548-0410-b2c8-1d8424a9ccce>
2008-03-22 23:46:50 +0000

    Avec un petit dessin, c'est bien.

--- trunk/subleq/KISS/kiss.ml
+++ trunk/subleq/KISS/kiss.ml
@@ -1,232 +1,177 @@
 (* ================================================================== *)
 (** KISS Is a SubLeq Simulator : simulateur d'execution d'une machine
-    SubLeq avec un mode Debug graphique sommaire *)
+    SubLeq avec un mode Debug graphique sommaire.
+    Permet de generer une famille Ai tel que Slc(Ai) termine en 
+    fonction d'une condition c. (à l'aide d'un time out naif) *)
 (* ================================================================== *)
 
-#load "graphics.cma";;
-open Graphics;;
+(* Utilise la bplib : ~/bebop1.0/boplibs/bplib.cmxa *)
+open Graphics
+open Bplib
+open Bitreader
 
-open_graph " 900x600";;
-close_graph ();;
-let clean_up () = 
-  set_color black;
-  fill_rect 0 0 900 600;;
+(* on utilise la class subleq machine en mode debug uniquement *)
 
-clean_up ();;
-
-set_window_title "Kiss : Debug Mode";;
-
-let digits_of_int nd i =
-  let s = String.make nd '0' in
-  let s2 = string_of_int i in
-  let len2 = String.length s2 in
-  for i = 0 to pred (min nd len2)  do
-    s.[nd-1-i] <- s2.[len2-1-i]
-  done;
-  s;;
-
-let center imin imax j mot = 
-  let (di, dj) = text_size mot in
-  let add = imin + ((imax-imin-di)/2) in
-  let x,y = current_point () in
-  moveto add j;
-  draw_string mot;
-  moveto x y;;
-  
-
-type style_boite = 
-{
-  fond_color : color;
-  num_digit : int;
-  text_color : color;
-  plateau_color : color
-};;
-
-type info_boite = 
-{
-  box_name : string;
-  coord : int * int;
-  style : style_boite
-}
-;;  
-
-(** l'interface des classes box d'affichage *)
-class type type_box = 
-object
-  val ib : info_boite
-  val mutable v : int
-  method set : int -> unit
-  method private draw : unit
-  method init : unit
-end
+let couleurPC = yellow 
+and couleurA = green
+and couleurB = magenta
+and couleurJMP = red
 
 ;;
 
-class box info : type_box = 
+class subleq_machine archi init =
 object(this)
-  val ib = info
-  val mutable v = 0
-  method private draw = 
+  val ram = new ram "Machine Subleq" archi archi init
+  val length = 2 *$ archi 
+  val mutable pc = 0
+
+  method condition_arret = pc = 1
+    
+  method draw = ram#draw
+
+  method run =
     begin
-      (* on efface *)
-      set_color ib.style.fond_color;
-      match ib.coord, ib.style.num_digit with
-      | (i,j), d -> 
-	  begin
-	    fill_rect i j ((d+2)*6) 13; 
-	    (* on reecrit le cadre *)
-	    set_color ib.style.plateau_color;
-	    draw_poly_line [|(i,j+13); (i,j); (i+(d+2)*6, j); (i+(d+2)*6, j+13)|];
-	    (* on ecrit la valeur *)
-	    set_color ib.style.text_color;
-	    moveto (i+6) (j+2);
-	    let vs = digits_of_int ib.style.num_digit v in
-	    draw_string vs;
-	    (* on ecrit le nom de la boite en dessous *)
-	    center i (i+(d+2)*6) (j-15) ib.box_name
-	  end
+      while not (this#condition_arret) do
+	this#iter
+      done;
+      prerr_endline "Condition d'arret, fin du calcul"
     end
 
-  method set x = 
-    if v = x 
-    then ()
-    else (v <- x;this#draw) 
-    
-  method init =
-    v <- 0;
-    this#draw
-end;;
-
-
-let style1 = 
-{ 
-  fond_color = black;
-  num_digit = 4;
-  text_color = red;
-  plateau_color = yellow
-};;
-
-let ibPC = 
-{
-  box_name = "pc";
-  coord = 180 , 580;
-  style = style1
-};;
-
-
-let ibA = 
-{
-  box_name = "A";
-  coord = 30 , 580;
-  style = style1
-};;
-
-let ibB =
-{
-  box_name = "B";
-  coord = 130 , 580;
-  style = style1
-};;
-
-let ibC =
-{
-  box_name = "C";
-  coord = 230 , 580;
-  style = style1
-};;
-
-let ibvA = 
-{
-  box_name = "*A";
-  coord = 30 , 550;
-  style = style1
-};;
-
-let ibvB =
-{
-  box_name = "*B";
-  coord = 130 , 550;
-  style = style1
-};;
-
-let ibvBA =
-{
-  box_name = "*B - *A";
-  coord = 230 , 550;
-  style = style1
-};;
-
-
-let boxA = new box ibA;;
-let boxB = new box ibB;;
-let boxC = new box ibC;;
-let boxvA = new box ibvA;;
-let boxvB = new box ibvB;;
-let boxvBA = new box ibvBA;;
-
-clean_up ();;
-boxA#init;;
-boxB#init;;
-boxC#init;;
-boxvA#init;;
-boxvB#init;;
-boxvBA#init;;
-
-
-type info_dump =
-{
-  style_d : style_boite;
-  largeur_col : int;
-  largeur_ligne : int;
-  num_col : int;
-  num_ligne : int;
-  debut : int * int;
-  cache : int * int * int * int
-}
-
-(** l'interface de la classe code_dump *)
-class type type_code_dump = 
-object
-  val code : int array
-  val mutable pc : int (* program counter *)
-  val mutable adressA : int
-  val mutable adressB : int
-  val mutable adressC : int
-  val mutable offset : int  (* l'indice de la premiere data affichee *)
-  method set_value : int -> int -> unit
-  method set_offset : int -> unit
-  method draw : unit
+  method iter = 
+    ram#reset_all_color;
+    let pc1 = ((pc+1) mod length) 
+    and pc2 = ((pc+2) mod length)
+    and pc3 = ((pc+3) mod length) in
+    let a = ram#read (pc mod length) 
+    and b = ram#read pc1 
+    and c = ram#read pc2 in
+    let a' = ram#read (a mod length)
+    and b' = ram#read (b mod length) in
+    let diff = (b' - a' + length) mod length in
+    ram#set_color pc couleurPC;
+    ram#set_color pc1 couleurPC;
+    ram#set_color pc2 couleurPC;
+    ram#set_color a couleurA;
+    ram#set_color b couleurB;
+    let neg = not (diff > 0 && diff < (length / 2)) in
+    (
+      if neg 
+      then ram#set_color c couleurJMP
+      else ram#set_color pc3 couleurJMP
+    ) ;
+    Printf.fprintf stderr "Ecrire dans %d le resultat de %d - %d = %d\n" b b' a' diff ;
+    Printf.fprintf stderr "Ce resultat est negatif ou nul : %b\n" neg;
+    flush stderr;
+    ram#wait;
+    ram#write b diff;
+    if neg 
+    then pc <- c
+    else pc <- pc3
 end
 
 
-class code_dump init fond ligne_color cx cy dcx dcy x y largeur_col hauteur_col 
-  num_col num_lign largeur_lign = 
-object(this)
-  val code = Array.copy init
-  val mutable pc = 0
-  val mutable adressA = 0
-  val mutable adressB = 0
-  val mutable adressC = 0
-  val mutable offset = 0
-  method set_value index (v:int) = code.(index) <- v
-  method set_offset no = offset <- no; this#draw
-    
-  method clean_up =
-    set_color fond;
-    fill_rect cx cy dcx dcy;
-    
+(* 
+   options 
+   -g       : graphique pas à pas
+   -b + arg : avec une initialisation binaire
+   arg      : avec une initialisation asci
+*)
 
-  method draw = 
-    this#clean_up;
-    set_color ligne_color;
-    for i = 0 to pred num_col do  
-      draw_rect (x+i*largeur_col) y largeur_col hauteur_col 
-    done;  
-end
+type execution =
+{
+  archi : int;
+  mode_graphique : bool;
+  init : string option;
+  typ : source
+} and source = Binary | Asci
 
-let cd = new code_dump (Array.init 500 (fun i -> i)) blue yellow 0 0 900 520
-50 10 100 500 8 30 15;;
 
-clean_up();;
-cd#clean_up;;
-cd#draw;;
+(* ===================================================================== *)
+(** Exeption levee quand la ligne de commande est incoherente *)
+exception Arguments_Invalides
+(* ===================================================================== *)
+
+(* ===================================================================== *)
+(** La chaine est-elle un entier ? *)
+let is_an_int s = try ignore (int_of_string s);true with Failure _ -> false
+(* ===================================================================== *)
+
+(* ===================================================================== *)
+(** Deduire d'un tableau Sys.argv le type d'execution
+    Lever l'exeption Arguments_Invalides si la lecture echoue *)
+(* ===================================================================== *)
+let execution_of_argv argv = 
+  let n = Array.length argv in
+  if n < 2 || n > 5
+  then raise Arguments_Invalides
+  else
+    let typ = ref Asci
+    and dbl = ref false
+    and graph = ref false
+    and archi = ref None
+    and queue_sources = Queue.create () 
+    and i = ref 1 in  
+    begin
+      while (!i < n) do
+        begin
+          let mot = argv.(!i) in
+          match mot with
+            (* La chaine vide *)
+          | "" -> ()
+              (* une option *)
+	  | "-g" -> graph := true
+	  | "-b" -> 
+	      if !dbl then raise Arguments_Invalides 
+	      else (dbl := true; typ := Binary)
+	  | "-a" -> 
+	      if !dbl then raise Arguments_Invalides 
+	      else (dbl := true; typ := Asci)
+        (* precision de l'archi *)
+          | m when (is_an_int m) -> 
+              archi := Some (int_of_string m)
+          | m -> Queue.add m queue_sources
+        end;
+        incr i;
+      done;
+      (* Verification de la coherence *)
+      if (!archi = None) || (Queue.is_empty queue_sources) 
+      then raise Arguments_Invalides
+      else 
+	match (!archi, !graph) with
+	| (Some a, g) -> 
+	    {
+	      archi = a;
+	      init = Some (Queue.pop queue_sources);
+	      mode_graphique = g;
+	      typ = !typ
+	    }
+	| _ -> raise Arguments_Invalides
+    end 
+(* ===================================================================== *)
+
+let _ = 
+  prerr_endline "KISS : Kiss Is a Subleq Simulator";
+  try
+    let exe = execution_of_argv Sys.argv in
+    let init_tab = 
+      match exe.init with
+      | Some file ->
+	  if exe.typ = Binary
+	  then BoolMatrix.of_bin_file (2 *$ exe.archi) exe.archi file
+	  else BoolMatrix.of_asci_file (2 *$ exe.archi) exe.archi file
+      | None -> 
+	  prerr_endline "Warning, you should give a file to init the Subleq Machine !";
+	  [||]
+    in
+    if exe.mode_graphique 
+    then
+      let sl = new subleq_machine exe.archi init_tab in 
+      begin
+	open_graph " 300x400";
+	sl#run
+      end
+    else ()
+  with
+  | Arguments_Invalides -> 
+      prerr_endline (Printf.sprintf "use : %s [-g] archit.(int) [-b/-a] file" Sys.argv.(0))
+  
